785cb8ec4e8e61571b70faeaa6cfbcd8a357b656
tharu12sa <tharushakavinda621@gmail.com>
2026-07-18 11:16:29 +0530

    Add number guessing game functionality and styling
    - Implemented random number generation and user input handling in app.js
    - Enhanced HTML structure for user interaction with input fields and output display
    - Added CSS for improved visual styling

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 console.log("Hi");
+
+let num = Math.random() * 10 + 1;
+let randomnum = Math.floor(num);
+
+let attemp = 0;
+
+
+console.log(randomnum);
+
+function btnAddsumbit() {
+    let txtInput = document.getElementById("inputText1").value;
+    attemp++;
+
+     if (attemp > 3) {
+        output.textContent = "Game Over ❌";
+        return;
+    }
+        
+    if (randomnum < txtInput) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: "<a href=\"#\">Why do I have this issue?</a>"
+        });
+        let output = document.getElementById("output");
+        output.textContent = "Your number is higher 😊"
+    } else if (randomnum > txtInput) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: "<a href=\"#\">Why do I have this issue?</a>"
+        });
+        let output = document.getElementById("output");
+        output.textContent = "Your number is less 😒"
+
+    } else {
+        let output = document.getElementById("output");
+        output.textContent = "you win 🎉"
+        return;
+    }
+
+
+   
+}
