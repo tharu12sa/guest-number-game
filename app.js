59b4d2a80cecda0517593dab44800240a8828cdc
tharu12sa <tharushakavinda621@gmail.com>
2026-07-21 11:31:34 +0530

    Fix image path for win notification backdrop in game logic

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function btnAddsumbit() {
             background: "#fff url(/images/trees.png)",
             backdrop: `
     rgba(0,0,123,0.4)
-    url("/img/win.png")
+    url("img/win.png")
     left top
     no-repeat
   `
