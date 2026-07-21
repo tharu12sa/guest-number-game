fedb75de0e58a2574b5c177874914c885a01af98
tharu12sa <tharushakavinda621@gmail.com>
2026-07-21 09:23:11 +0530

    Refactor game logic and improve UI elements for number guessing game

--- app.js
+++ app.js
@@ -10,38 +10,25 @@ console.log(randomnum);
 
 function btnAddsumbit() {
     let txtInput = document.getElementById("inputText1").value;
+    let output = document.getElementById("output");
+    let submitbtn = document.getElementById("btnClose");
+   
     attemp++;
 
-     if (attemp > 3) {
-        output.textContent = "Game Over ❌";
-        return;
-    }
-        
-    if (randomnum < txtInput) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Something went wrong!",
-            footer: "<a href=\"#\">Why do I have this issue?</a>"
-        });
-        let output = document.getElementById("output");
-        output.textContent = "Your number is higher 😊"
-    } else if (randomnum > txtInput) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Something went wrong!",
-            footer: "<a href=\"#\">Why do I have this issue?</a>"
-        });
-        let output = document.getElementById("output");
-        output.textContent = "Your number is less 😒"
-
-    } else {
-        let output = document.getElementById("output");
-        output.textContent = "you win 🎉"
+    if(randomnum==txtInput){
+        output.textContent="You win 😍";
         return;
     }
 
+    if(attemp>=3){
+        output.textContent="You've run out of attempts. Please try again.";
+        return;
+    }
 
-   
+    if(randomnum>txtInput){
+        output.textContent="Your number is less 😒";
+    }else if(randomnum<txtInput){
+        output.textContent="Your number is higher 😊"
+    }
+
 }
